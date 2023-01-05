e9bcd386296bc1f4533a38e8bab6577b413b625d
yuan <33422287+YUANI12580@users.noreply.github.com>
2023-01-05 10:05:59 +0800

    Update train.py

--- train.py
+++ train.py
@@ -9,22 +9,6 @@ import numpy as np
 import torch
 
 
-# Copy from OpenGait
-def init_seeds(seed=0, cuda_deterministic=True):
-    random.seed(seed)
-    np.random.seed(seed)
-    torch.manual_seed(seed)
-    torch.cuda.manual_seed_all(seed)
-    # 3DLocal Not
-    # Speed-reproducibility tradeoff https://pytorch.org/docs/stable/notes/randomness.html
-    if cuda_deterministic:  # slower, more reproducible
-        torch.backends.cudnn.deterministic = True
-        torch.backends.cudnn.benchmark = False
-    else:  # faster, less reproducible
-        torch.backends.cudnn.deterministic = False
-        torch.backends.cudnn.benchmark = True
-
-
 def boolean_string(s):
     if s.upper() not in {'FALSE', 'TRUE'}:
         raise ValueError('Not a valid boolean string')
@@ -37,9 +21,6 @@ parser.add_argument('--cache', default=True, type=boolean_string,
                     help='cache: if set as TRUE all the training data will be loaded at once'
                          ' before the training start. Default: TRUE')
 opt = parser.parse_args()
-
-# Copy from OpenGait
-init_seeds()
 
 m = initialization(conf, train=opt.cache)[0]
 print(conf)
